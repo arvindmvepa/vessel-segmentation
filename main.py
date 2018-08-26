9f4201a1c2aeb8f3f7db08a67724eaffdaf99ed8
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-08-26 04:32:42 -0700

    small update

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-from job.stare import StareJob
+from job.dsa import DsaJob
 from job.drive import DriveJob
-from job.hrf import HRFJob
+from job.stare import StareJob
+from job.chase import ChaseJob
+
 
 import os
 
 if __name__ == '__main__':
-    experiments_dir_path = "/root/vessel-seg/data/STARE"
+    experiments_dir_path = "/root/vessel-seg"
     experiment_name = "test"
     job = StareJob(OUTPUTS_DIR_PATH=os.path.join(experiments_dir_path, experiment_name))
-    job.run_cross_validation(WRK_DIR_PATH="/root/vessel-seg/data/STARE",metrics_epoch_freq=1,
-                             viz_layer_epoch_freq=1,n_epochs=5, gpu_device='/gpu:1' )
-    print("done")
-
-#    experiments_dir_path = "/root/vessel-seg/data/HRF"
-#    experiment_name = "test"
-#    job = HRFJob(OUTPUTS_DIR_PATH=os.path.join(experiments_dir_path, experiment_name))
-#    job.run_cross_validation(WRK_DIR_PATH="/root/vessel-seg/data/HRF",metrics_epoch_freq=1,
-#                         viz_layer_epoch_freq=1,n_epochs=5, gpu_device='/gpu:1')
-#    print("done")
+    job.run_cross_validation(WRK_DIR_PATH="/root/vessel-seg/data/stare", metrics_epoch_freq=1, viz_layer_epoch_freq=1,
+                             n_epochs=5)
