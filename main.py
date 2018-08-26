5a6d916ab34f52a36beef10a249ed9b9687242c6
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-08-26 04:32:42 -0700

    small update

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from job.stare import StareJob
+from job.dsa import DsaJob
 from job.drive import DriveJob
-from job.hrf import HRFJob
+from job.stare import StareJob
+from job.chase import ChaseJob
+
 
 import os
 
 if __name__ == '__main__':
     EXPERIMENTS_DIR_PATH = "/Users/arvind.m.vepa/vessel_seg_data/experiments"
-    EXPERIMENT_NAME = "drive_example_"
+    EXPERIMENT_NAME = "drive_example"
     job = DriveJob(OUTPUTS_DIR_PATH=os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME))
     # keyword arguments are passed to several methods, please look at the code to see the flow of key-word arguments
     job.run_cross_validation(WRK_DIR_PATH="/Users/arvind.m.vepa/vessel_seg_data/drive",metrics_epoch_freq=None,
