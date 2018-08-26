c3755e11ec12d7f19683de018a6fc36b60902580
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-08-26 02:01:23 -0700

    bug

--- dataset/dsa.py
+++ dataset/dsa.py
@@ -13,7 +13,7 @@ class DsaDataset(DatasetWoMasks):
 
     def __init__(self, batch_size=1, WRK_DIR_PATH ="./dsa", TRAIN_SUBDIR="train", TEST_SUBDIR="test", sgd = True,
                  cv_train_inds = None, cv_test_inds = None):
-        super(DatasetWoMasks, self).__init__(batch_size=batch_size, WRK_DIR_PATH=WRK_DIR_PATH, TRAIN_SUBDIR=TRAIN_SUBDIR,
+        super(DsaDataset, self).__init__(batch_size=batch_size, WRK_DIR_PATH=WRK_DIR_PATH, TRAIN_SUBDIR=TRAIN_SUBDIR,
                                          TEST_SUBDIR=TEST_SUBDIR, sgd=sgd, cv_train_inds=cv_train_inds,
                                          cv_test_inds=cv_test_inds)
 
