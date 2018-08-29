2f70f9a1d50adc674f5425cdb690288b74443ee3
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-08-28 21:44:37 -0700

    change stare threshold

--- dataset/stare.py
+++ dataset/stare.py
@@ -4,7 +4,7 @@ from network.stare import StareNetwork
 class StareDataset(DatasetWMasks):
 
     def __init__(self, batch_size=1, WRK_DIR_PATH ="./stare", TRAIN_SUBDIR="train", TEST_SUBDIR="test", sgd = True,
-                 masks_provided=False, mask_threshold=.5, cv_train_inds = None, cv_test_inds = None):
+                 masks_provided=False, mask_threshold=.1, cv_train_inds = None, cv_test_inds = None):
         super(StareDataset, self).__init__(batch_size=batch_size, WRK_DIR_PATH=WRK_DIR_PATH, TRAIN_SUBDIR=TRAIN_SUBDIR,
                                            TEST_SUBDIR=TEST_SUBDIR, sgd=sgd, masks_provided=masks_provided,
                                            mask_threshold=mask_threshold, cv_train_inds=cv_train_inds,
