ea9b98e967f9afef0b3017e831e6028c1d80ac7a
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-02 23:36:17 -0700

    bug

--- utilities/objective_functions.py
+++ utilities/objective_functions.py
@@ -171,8 +171,7 @@ def cross_entropy(prediction, ground_truth, weight_map=None):
     print(ground_truth.shape)
     print(prediction.shape)
 
-    #ground_truth = tf.expand_dims(ground_truth, axis=3)
-    #ground_truth = tf.stack([ground_truth,1-ground_truth], axis=3)
+    ground_truth = tf.stack([ground_truth,1-ground_truth], axis=3)
     prediction = tf.concat([prediction,1-prediction], axis=3)
 
     print(ground_truth.shape)
