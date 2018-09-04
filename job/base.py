4d3902e0ba254cadd5ad89d0d563eab2cc2e95c4
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-03 21:17:02 -0700

    print outputs

--- job/base.py
+++ job/base.py
@@ -269,19 +269,18 @@ class Job(object):
                         self.save_debug2(batch_data, viz_layer_outputs_path_train)
 
                     # train on batch
-                    cost, cost_unweighted, layer_outputs, debug1, _, cur_objective_fn, regularization, \
+                    cost, cost_unweighted, layer_outputs, debug1, _, regularization, \
                     cur_learning_rate = sess.run(
                         [network.cost, network.cost_unweighted, network.layer_outputs, network.debug1,
-                         network.train_op, network.cur_objective_fn, network.regularization,
-                         network.cur_learning_rate],
+                         network.train_op, network.regularization, network.cur_learning_rate],
                         feed_dict=self.get_network_dict(network, batch_data))
                     end = time.time()
                     # print training information
                     print('{}/{}, epoch: {}, cost: {}, cost unweighted: {}, batch time: {}, positive_weight: {},'
                           ' current objective function {}, regularization {}, regularizer {}, learning rate {}, '
                           'op function'.format(batch_num, self.n_epochs * dataset.num_batches_in_epoch(), epoch_i, cost,
-                                               cost_unweighted, end-start, pos_weight, cur_objective_fn, regularization,
-                                               network.regularizer, cur_learning_rate, network.cur_op_fn))
+                                               cost_unweighted, end-start, pos_weight, network.cur_objective_fn,
+                                               regularization, network.regularizer, cur_learning_rate, network.cur_op_fn))
 
                     # produce debug image 3
                     if viz_layer_epoch_freq is not None and debug_net_output:
