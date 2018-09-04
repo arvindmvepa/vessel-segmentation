5f8299ae15c54afed33d3c55e8bf8e1376fc8579
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-03 22:35:56 -0700

    print outputs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     EXPERIMENT_NAME = "drive_example"
 
     arg_index = 1
-    objective_fn, tuning_constant, ss_r, regularizer_args, learning_rate_and_kwargs, op_fun_and_kwargs, weight_init, act_fn, hist_eq,clahe_kwargs,per_image_normalization,gamma,seq = zip(["wce","gdice", "ss"], [1.0,1.0,1.0], [.05,.05,.05], [None, ("L1",.01),("L2",.01)], [(.001, {}), (.001, {}),(.01, {'decay_steps': 1, 'decay_rate': .1})], [("adam",{}),("rmsprop",{}),("adadelta",{})], ["He", "Xnormal", "default"], ["lrelu", "relu", "relu"], [False, False, True], [None, {"clipLimit": 2.0, "tileGridSize": (8, 8)}, {"clipLimit": 2.0, "tileGridSize": (8, 8)}], [False, True, True], [None, 1.0, 5.0], [None, None, iaa.Sequential([iaa.Crop(px=(0, 16)), iaa.Fliplr(0.5),iaa.GaussianBlur(sigma=(0, 3.0))])])[1]
+    objective_fn, tuning_constant, ss_r, regularizer_args, learning_rate_and_kwargs, op_fun_and_kwargs, weight_init, act_fn, hist_eq,clahe_kwargs,per_image_normalization,gamma,seq = zip(["wce","gdice", "ss"], [1.0,1.0,1.0], [.05,.05,.05], [None, ("L1",.000001),("L2",.01)], [(.001, {}), (.001, {}),(.01, {'decay_steps': 1, 'decay_rate': .1})], [("adam",{}),("rmsprop",{}),("adadelta",{})], ["He", "Xnormal", "default"], ["lrelu", "relu", "relu"], [False, False, True], [None, {"clipLimit": 2.0, "tileGridSize": (8, 8)}, {"clipLimit": 2.0, "tileGridSize": (8, 8)}], [False, True, True], [None, 1.0, 5.0], [None, None, iaa.Sequential([iaa.Crop(px=(0, 16)), iaa.Fliplr(0.5),iaa.GaussianBlur(sigma=(0, 3.0))])])[1]
     job = DriveJob(OUTPUTS_DIR_PATH=os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME))
     job.run_cross_validation(WRK_DIR_PATH="/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive",
                              metrics_epoch_freq=1,viz_layer_epoch_freq=1,
