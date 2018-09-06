0999679e40bf5cba0608638f38c5747bb45c04d0
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-06 02:16:28 -0700

    bug

--- job/base.py
+++ job/base.py
@@ -275,6 +275,7 @@ class Job(object):
                         feed_dict=self.get_network_dict(network, batch_data))
                     end = time.time()
                     # print training information
+                    print(learning_rate_and_kwargs)
                     print('{}/{}, epoch: {}, cost: {}, cost unweighted: {}, batch time: {}, positive_weight: {}, '
                           'regularizer type {}, op function {}, learning rate {}, regularization {}'.format(
                         batch_num, self.n_epochs * dataset.num_batches_in_epoch(), epoch_i, cost, cost_unweighted,
