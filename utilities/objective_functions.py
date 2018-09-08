5e1e8a968669b3c92ee31979878b4dd9755b8370
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-07 22:54:06 -0700

    test

--- utilities/objective_functions.py
+++ utilities/objective_functions.py
@@ -114,25 +114,6 @@ def sensitivity_specificity_loss(prediction, ground_truth, weight_map=None, r=0.
     one_hot = tf.sparse_tensor_to_dense(one_hot)
     squared_error = tf.square(one_hot - prediction)
 
-    """
-    # value of unity everywhere except for the previous 'hot' locations
-    one_cold = 1 - one_hot
-
-    print("ss output")
-    print(one_hot.shape)
-    print(squared_error.shape)
-    print(tf.reduce_sum(squared_error * one_hot, [0,1,2]).shape)
-    print(tf.reduce_sum(tf.multiply(squared_error, one_cold)).shape)
-
-    specificity_part = tf.reduce_sum(squared_error * one_hot) / \
-                       (tf.reduce_sum(one_hot) + epsilon_denominator)
-
-    sensitivity_part = (tf.reduce_sum(tf.multiply(squared_error, one_cold)) /
-                        (tf.reduce_sum(one_cold) + epsilon_denominator))
-
-    return tf.reduce_sum(r * specificity_part + (1 - r) * sensitivity_part)
-    """
-
     return tf.reduce_sum(tf.reduce_sum(squared_error * one_hot, [0,1,2]) /
                          (tf.reduce_sum(one_hot, [0,1,2]) + epsilon_denominator) * tf.constant([1-r,r]))
 
