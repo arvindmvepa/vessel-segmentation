62db24225ed24bf384fe077dd08f360477a6b1f6
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-08 04:09:08 -0700

    update

--- main.py
+++ main.py
@@ -8,23 +8,6 @@ import os
 
 if __name__ == '__main__':
 
-    """
-    args_index = 2
-    objective_fn, tuning_constant, ss_r, regularizer_args, learning_rate_and_kwargs, op_fun_and_kwargs, weight_init, \
-    act_fn, hist_eq,clahe_kwargs,per_image_normalization,gamma,seq = zip(["wce","gdice", "gdice"], [1.0,1.0,.50],
-                                                                         [.05,.05,.05],
-                                                                         [None, ("L1",.000001),None],
-                                                                         [(.001, {}), (.001, {}),
-                                                                          (.001, {})],
-                                                                         [("adam",{}),("rmsprop",{}),("adadelta",{})],
-                                                                         ["He", "Xnormal", "default"],
-                                                                         ["lrelu", "relu", "lrelu"],
-                                                                         [False, False, False],
-                                                                         [None, {"clipLimit": 2.0,"tileGridSize":(8,8)},
-                                                                          None],
-                                                                         [False, True, False],[None, 1.0, 1.0],
-                                                                         [None, None,None])[args_index]
-    """
     ### OPTIMIZATION
     objective_fn = "wce"
     tuning_constant = 1.0
@@ -32,7 +15,7 @@ if __name__ == '__main__':
     regularizer_args = None
     learning_rate_and_kwargs = (.01, {"decay_epochs":10,"decay_rate":.1,"staircase":False})
     #learning_rate_and_kwargs = (.001, {})
-    op_fun_and_kwargs = ("rmsprop",{})
+    op_fun_and_kwargs = ("adam",{})
 
     ### LAYER ARGS
     weight_init = "He"
