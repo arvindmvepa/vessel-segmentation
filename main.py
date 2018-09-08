f3019b7e8ac5926de275947c6ed389d77d82e743
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-08 00:29:11 -0700

    new args

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ import os
 
 if __name__ == '__main__':
 
-    EXPERIMENTS_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/experiments"
-    EXPERIMENT_NAME = "drive_example"
-
-    #example key-word arguments
+    """
     args_index = 2
     objective_fn, tuning_constant, ss_r, regularizer_args, learning_rate_and_kwargs, op_fun_and_kwargs, weight_init, \
     act_fn, hist_eq,clahe_kwargs,per_image_normalization,gamma,seq = zip(["wce","gdice", "gdice"], [1.0,1.0,.50],
@@ -27,13 +24,49 @@ if __name__ == '__main__':
                                                                           None],
                                                                          [False, True, False],[None, 1.0, 1.0],
                                                                          [None, None,None])[args_index]
-    job = DriveJob(OUTPUTS_DIR_PATH=os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME))
-    job.run_cross_validation(WRK_DIR_PATH="/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive",
-                             metrics_epoch_freq=1,viz_layer_epoch_freq=101,
-                             n_epochs=30,n_splits=2,objective_fn=objective_fn,
+    """
+    ### OPTIMIZATION
+    objective_fn = "wce"
+    tuning_constant = 1.0
+    ss_r = None
+    regularizer_args = None
+    learning_rate_and_kwargs = (.01, {"decay_steps":10,"decay_rate":.1})
+    op_fun_and_kwargs = ("adam",{})
+
+    ### LAYER ARGS
+    weight_init = "He"
+    act_fn = "lrelu"
+
+    ### IMAGE PRE-PREPROCESSING
+    hist_eq = False
+    clahe_kwargs = None
+    per_image_normalization = False
+    gamma = None
+    seq = iaa.Sequential([
+        iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
+        iaa.Fliplr(0.5), # horizontally flip 50% of the images
+        iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
+        ])
+
+    ### OUTPUT INFO
+    EXPERIMENTS_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/experiments"
+    EXPERIMENT_NAME = "example"
+    OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
+    metrics_epoch_freq = 1
+    viz_layer_epoch_freq = 101
+    n_epochs = 30
+
+    ### JOB INFO
+    WRK_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive"
+    n_splits = 2
+
+    job = DriveJob(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
+    job.run_cross_validation(WRK_DIR_PATH=WRK_DIR_PATH,
+                             metrics_epoch_freq=metrics_epoch_freq,viz_layer_epoch_freq=viz_layer_epoch_freq,
+                             n_epochs=n_epochs,n_splits=n_splits,objective_fn=objective_fn,
                              tuning_constant=tuning_constant, ss_r=ss_r,
                              regularizer_args=regularizer_args,
-                             op_fun_and_kwargs=("adam",{}),
+                             op_fun_and_kwargs=op_fun_and_kwargs,
                              learning_rate_and_kwargs=learning_rate_and_kwargs,
                              weight_init=weight_init, act_fn=act_fn,
                              seq=seq, hist_eq=hist_eq,
