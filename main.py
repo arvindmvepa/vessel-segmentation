4534b78a78c03c3e6c7abe834e099eb18a034fca
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-16 18:07:10 -0700

    bug

--- main.py
+++ main.py
@@ -9,64 +9,62 @@ import os
 
 if __name__ == '__main__':
 
-    num_searches = 1
-    EXPERIMENTS_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/experiments2"
+    ### OPTIMIZATION
+    op_fun_and_kwargs = ("adam", {})
+    tuning_constant = 1.0
+    ss_r = .5
+    objective_fn = "wce"
+    regularizer_args = None
+    learning_rate_and_kwargs = (.01, {})
 
+    ### LAYER ARGS
+    weight_init = "He"
+    act_fn = "lrelu"
+    act_leak_prob = 0.2
+
+    ### IMAGE PRE-PREPROCESSING
+    hist_eq = False
+    clahe_kwargs = None
+    per_image_normalization = False
+    gamma = 1.0
+
+    ### IMAGE AUGMENTATION
+    # this image augmentation setting doesn't seem to be good
+    """
+    seq = iaa.Sequential([
+        iaa.Crop(px=(0, 16)), # crop images from each side by 0 to 16px (randomly chosen)
+        iaa.Fliplr(0.5), # horizontally flip 50% of the images
+        iaa.GaussianBlur(sigma=(0, 3.0)) # blur images with a sigma of 0 to 3.0
+        ])
+    """
+    seq = None
+
+    ### JOB INFO
+    WRK_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive"
+    n_splits = 3
+
+    ### OUTPUT INFO
+    EXPERIMENTS_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/experiments2"
+    EXPERIMENT_NAME = str((objective_fn,tuning_constant,ss_r if objective_fn=="ss" else None,regularizer_args,op_fun_and_kwargs,
+                           learning_rate_and_kwargs, weight_init, act_fn, act_leak_prob, False if seq is None else True,
+                           hist_eq, clahe_kwargs, per_image_normalization,gamma))
+    OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
     metrics_epoch_freq = 1
     viz_layer_epoch_freq = 101
     n_epochs = 5
 
-    WRK_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive"
-    n_splits = 3
-
-    ### RANDOM SEARCH
-    tuning_constants = [1.0]
-    ss_rs = [.5]
-    objective_fns = ["wce"]
-    regularizer_argss = [None]
-    learning_rate_and_kwargss = [(.01, {})]
-
-    op_fun_and_kwargss = [("adam", {})]
-    weight_inits = ["He"]
-    act_fns = ["lrelu"]
-    act_leak_probs = [0.2]
-
-    hist_eqs = [True,False]
-
-    clahe_kwargss = [None]
-
-    per_image_normalizations = [False]
-    gammas = [1.0]
-
-    seqs = [None]
-    total_hyper_parameter_combos = list(product(tuning_constants, ss_rs, objective_fns,
-                                                regularizer_argss, learning_rate_and_kwargss,
-                                                op_fun_and_kwargss, weight_inits, act_fns,
-                                                act_leak_probs, hist_eqs, clahe_kwargss,
-                                                per_image_normalizations, gammas, seqs))
-    cur_hyper_parameter_combos = sample(total_hyper_parameter_combos, num_searches)
-
-    for tuning_constant, ss_r, objective_fn, regularizer_args, learning_rate_and_kwargs, op_fun_and_kwargs, weight_init,\
-        act_fn, act_leak_prob, hist_eq, clahe_kwargs, per_image_normalization, gamma, seq in \
-            cur_hyper_parameter_combos:
-
-        EXPERIMENT_NAME = str((objective_fn,tuning_constant,ss_r if objective_fn=="ss" else None,regularizer_args,op_fun_and_kwargs,
-                               learning_rate_and_kwargs, weight_init, act_fn, act_leak_prob, False if seq is None else True,
-                               hist_eq, clahe_kwargs, per_image_normalization,gamma))
-        OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
-
-        job = DriveJob(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
-        job.run_cv(WRK_DIR_PATH=WRK_DIR_PATH, mc=True, early_stopping=True, early_stopping_metric="auc",
-                   save_model=False, save_sample_test_images=False,
-                   metrics_epoch_freq=metrics_epoch_freq, viz_layer_epoch_freq=viz_layer_epoch_freq,
-                   n_epochs=n_epochs, n_splits=n_splits, objective_fn=objective_fn,
-                   tuning_constant=tuning_constant, ss_r=ss_r,
-                   regularizer_args=regularizer_args,
-                   op_fun_and_kwargs=op_fun_and_kwargs,
-                   learning_rate_and_kwargs=learning_rate_and_kwargs,
-                   weight_init=weight_init, act_fn=act_fn, act_leak_prob=act_leak_prob,
-                   seq=seq, hist_eq=hist_eq,
-                   clahe_kwargs=clahe_kwargs,
-                   per_image_normalization=per_image_normalization,
-                   gamma=gamma)
+    job = DriveJob(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
+    job.run_cv(WRK_DIR_PATH=WRK_DIR_PATH, mc=True, early_stopping=True, early_stopping_metric="auc",
+               save_model=False, save_sample_test_images=False,
+               metrics_epoch_freq=metrics_epoch_freq, viz_layer_epoch_freq=viz_layer_epoch_freq,
+               n_epochs=n_epochs, n_splits=n_splits, objective_fn=objective_fn,
+               tuning_constant=tuning_constant, ss_r=ss_r,
+               regularizer_args=regularizer_args,
+               op_fun_and_kwargs=op_fun_and_kwargs,
+               learning_rate_and_kwargs=learning_rate_and_kwargs,
+               weight_init=weight_init, act_fn=act_fn, act_leak_prob=act_leak_prob,
+               seq=seq, hist_eq=hist_eq,
+               clahe_kwargs=clahe_kwargs,
+               per_image_normalization=per_image_normalization,
+               gamma=gamma)
 
