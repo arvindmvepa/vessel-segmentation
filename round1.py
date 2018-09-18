7fb8bdeb14e3513c627f5eb6cd01083417348795
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-09-17 22:08:39 -0700

    new round of hyper-parameter tests

---
+++ round1.py
@@ -0,0 +1,82 @@
+from job.dsa import DsaJob
+from job.drive import DriveJob
+from job.stare import StareJob
+from job.chase import ChaseJob
+from imgaug import augmenters as iaa
+from itertools import product
+from random import sample
+import os
+
+if __name__ == '__main__':
+
+    num_searches = 20
+    EXPERIMENTS_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/experiments2"
+
+    metrics_epoch_freq = 5
+    viz_layer_epoch_freq = 101
+    n_epochs = 100
+
+    WRK_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive"
+    n_splits = 4
+
+    ### RANDOM SEARCH
+    tuning_constants = [.2,.5,1.0,1.5,2.0]
+    ss_rs = [.166,.33,.5,.6,.667]
+    objective_fns = ["wce","gdice","ss"]
+    regularizer_argss = [None, None,None, None,("L1",1E-8),("L1",1E-6),("L2",1E-8),("L2",1E-6),
+                        ("L2",1E-4),("L2",1E-2)]
+    learning_rate_and_kwargss = [(.1, {"decay_epochs":5,"decay_rate":.1,"staircase":False}),
+                                 (.1, {"decay_epochs":5,"decay_rate":.1,"staircase":True}),
+                                 (.1, {"decay_epochs": 10, "decay_rate": .1, "staircase": False}),
+                                 (.1, {"decay_epochs": 10, "decay_rate": .1, "staircase": True}),
+                                 (.1, {}),
+                                 (.01, {}),
+                                 (.001, {})]
+
+    op_fun_and_kwargss = [("adam", {}), ("grad", {}), ("adagrad", {}), ("adadelta", {}), ("rmsprop", {})]
+    weight_inits = ["default","He","Xnormal"]
+    act_fns = ["lrelu"]
+    act_leak_probs = [0.0,0.0,0.2,.2,0.4,0.6]
+
+    hist_eqs = [True,False]
+
+    clahe_kwargss = [None, None, None, None, None,
+                     {"clipLimit": 2.0,"tileGridSize":(8,8)}, {"clipLimit": 2.0,"tileGridSize":(4,4)},
+                     {"clipLimit": 2.0,"tileGridSize":(16,16)}, {"clipLimit": 20.0, "tileGridSize": (8, 8)},
+                     {"clipLimit": 60.0, "tileGridSize": (8, 8)}]
+
+    per_image_normalizations = [False, True]
+    gammas = [1.0,1.0,1.0,2.0,4.0,6.0]
+
+    seqs = [None]
+
+    total_hyper_parameter_combos = list(product(tuning_constants, ss_rs, objective_fns, regularizer_argss, learning_rate_and_kwargss,
+                                                op_fun_and_kwargss, weight_inits, act_fns, act_leak_probs, hist_eqs, clahe_kwargss,
+                                                per_image_normalizations, gammas, seqs))
+
+    cur_hyper_parameter_combos = sample(total_hyper_parameter_combos, num_searches)
+
+    for tuning_constant, ss_r, objective_fn, regularizer_args, learning_rate_and_kwargs, op_fun_and_kwargs, weight_init,\
+        act_fn, act_leak_prob, hist_eq, clahe_kwargs, per_image_normalization, gamma, seq in \
+            cur_hyper_parameter_combos:
+
+        EXPERIMENT_NAME = str((objective_fn,tuning_constant,ss_r if objective_fn=="ss" else None,regularizer_args,op_fun_and_kwargs,
+                               learning_rate_and_kwargs, weight_init, act_fn, act_leak_prob, False if seq is None else True,
+                               hist_eq, clahe_kwargs, per_image_normalization,gamma))
+        OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
+
+        job = DriveJob(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
+        job.run_cv(WRK_DIR_PATH=WRK_DIR_PATH, mc=True, val_prop=.10, early_stopping=False, early_stopping_metric="auc",
+                   save_model=False, save_sample_test_images=False,
+                   metrics_epoch_freq=metrics_epoch_freq, viz_layer_epoch_freq=viz_layer_epoch_freq,
+                   n_epochs=n_epochs, n_splits=n_splits, objective_fn=objective_fn,
+                   tuning_constant=tuning_constant, ss_r=ss_r,
+                   regularizer_args=regularizer_args,
+                   op_fun_and_kwargs=op_fun_and_kwargs,
+                   learning_rate_and_kwargs=learning_rate_and_kwargs,
+                   weight_init=weight_init, act_fn=act_fn, act_leak_prob=act_leak_prob,
+                   seq=seq, hist_eq=hist_eq,
+                   clahe_kwargs=clahe_kwargs,
+                   per_image_normalization=per_image_normalization,
+                   gamma=gamma)
+
