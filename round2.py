4c52bb80ec683cb750653e6c805aa5bbd894d3a2
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-10-14 06:21:19 -0700

    for ubuntu

--- round2.py
+++ round2.py
@@ -54,15 +54,15 @@ def get_experiment_string(objective_fn,tuning_constant,ss_r,regularizer_args,op_
 if __name__ == '__main__':
 
     num_searches = 20
-    #EXPERIMENTS_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/experiments3"
-    EXPERIMENTS_DIR_PATH = "C:\\vessel-segmentation\\experiments3"
+    EXPERIMENTS_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/experiments3"
+    #EXPERIMENTS_DIR_PATH = "C:\\vessel-segmentation\\experiments3"
 
     metrics_epoch_freq = 5
     viz_layer_epoch_freq = 101
     n_epochs = 100
 
-    #WRK_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive"
-    WRK_DIR_PATH = "C:\\vessel-segmentation\\drive"
+    WRK_DIR_PATH = "/home/ubuntu/new_vessel_segmentation/vessel-segmentation/drive"
+    #WRK_DIR_PATH = "C:\\vessel-segmentation\\drive"
     n_splits = 4
 
     ### RANDOM SEARCH
