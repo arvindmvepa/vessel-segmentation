470bb412ae4cb72f23c2e836aae201f6fdad5722
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-10-14 04:56:59 -0700

    bug

--- round2.py
+++ round2.py
@@ -10,13 +10,13 @@ import json
 
 def get_experiment_string(objective_fn,tuning_constant,ss_r,regularizer_args,op_fun_and_kwargs,
                           learning_rate_and_kwargs, weight_init, act_fn, act_leak_prob, seq, hist_eq, clahe_kwargs,
-                          per_image_normalization,gamma):
+                          per_image_normalization,gamma, sep="*"):
     exp_string = ""
-    exp_string += objective_fn + "|"
-    exp_string += str(tuning_constant) + "|"
-    exp_string += str(ss_r) if objective_fn=="ss" else str(None) + "|"
-    exp_string += str(regularizer_args) + "|"
-    exp_string += str(op_fun_and_kwargs) + "|"
+    exp_string += objective_fn + sep
+    exp_string += str(tuning_constant) + sep
+    exp_string += str(ss_r) if objective_fn=="ss" else str(None) + sep
+    exp_string += str(regularizer_args) + sep
+    exp_string += str(op_fun_and_kwargs) + sep
 
     learning_rate_kwargs = learning_rate_and_kwargs[1]
     if "decay_epochs" in learning_rate_kwargs:
@@ -25,22 +25,22 @@ def get_experiment_string(objective_fn,tuning_constant,ss_r,regularizer_args,op_
         learning_rate_kwargs["d_r"] = learning_rate_kwargs.pop("decay_rate")
     if "staircase" in learning_rate_kwargs:
         learning_rate_kwargs["s_c"] = learning_rate_kwargs.pop("staircase")
-    exp_string += "("+str(learning_rate_and_kwargs[0]) +","+json.dumps(learning_rate_kwargs, sort_keys=True) + ")|"
+    exp_string += "("+str(learning_rate_and_kwargs[0]) +","+json.dumps(learning_rate_kwargs, sort_keys=True) + ")"+sep
 
-    exp_string += weight_init + "|"
-    exp_string += act_fn + "|"
-    exp_string += str(act_leak_prob) + "|"
-    exp_string += str(seq)
-    exp_string += str(hist_eq) + "|"
+    exp_string += weight_init + sep
+    exp_string += act_fn + sep
+    exp_string += str(act_leak_prob) + sep
+    exp_string += str(seq) + sep
+    exp_string += str(hist_eq) + sep
 
     if clahe_kwargs is None:
-        exp_string += str(None) + "|"
+        exp_string += str(None) + sep
     else:
         clahe_kwargs["cl"] = clahe_kwargs.pop("clipLimit")
         clahe_kwargs["tgs"] = clahe_kwargs.pop("tileGridSize")
-        exp_string += json.dumps(clahe_kwargs, sort_keys=True) + "|"
+        exp_string += json.dumps(clahe_kwargs, sort_keys=True) + sep
 
-    exp_string += str(per_image_normalization) + "|"
+    exp_string += str(per_image_normalization) + sep
     exp_string += str(gamma)
 
     return exp_string
