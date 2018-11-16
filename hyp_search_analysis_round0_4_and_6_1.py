c0fa798c56086bf084633037b01d69d983dc67a7
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-11-15 22:33:18 -0800

    best models round

--- hyp_search_analysis_round0_4_and_6_1.py
+++ hyp_search_analysis_round0_4_and_6_1.py
@@ -1,6 +1,15 @@
+#from imgaug import augmenters as iaa
+import csv
+from collections import defaultdict
+import numpy as np
+import re
+from utilities.misc import remove_duplicates
+from statsmodels import robust
 from copy import deepcopy
 import json
-from utilities.misc import remove_duplicates, copy_jobs, analyze
+import os
+
+from utilities.misc import remove_duplicates, copy_jobs, analyze, get_job_kwargs_from_job_opts, get_job_opts
 
 def clean_str(val):
     str_val = str(val)
@@ -15,21 +24,30 @@ def get_hyp_opts(all_hyps = ("objective_fns", "tuning_constants", "ss_rs", "regu
 
     # hyper-parameter search space
     tuning_constants = [.5,1.0,1.5,2.0]
-    ss_rs = [.166,.5,.6,.667]
+    ss_rs = [.166,.5,.6,.63,.667]
     objective_fns = ["wce","gdice","ss"]
 
-    regularizer_argss = [None, None, None,("L1",1E-6), ("L1",1E-8), ("L2",1E-4),("L2",1E-6),("L2",1E-6),("L2",1E-6), ("L2",1E-8)]
+    regularizer_argss = [None,("L1",1E-6), ("L1",1E-8), ("L2",1E-4),("L2",1E-6),("L2",1E-6),("L2",1E-6), ("L2",1E-8)]
+
     learning_rate_and_kwargss = [(.1, {"decay_epochs":5,"decay_rate":.1,"staircase":False}),
                                  (.1, {"decay_epochs":5,"decay_rate":.1,"staircase":True}),
-                                 (.1, {"decay_epochs": 10, "decay_rate": .1, "staircase": True}),
+                                 (.1, {"decay_epochs": 10, "decay_rate": .1, "staircase": False}),
                                  (.1, {}),
                                  (.01, {}),
-                                 (.001, {})]
+                                 (.1, {"decay_epochs": 10, "decay_rate": .1, "staircase": True}),
+                                 (.01, {"decay_epochs": 25, "decay_rate": .1, "staircase": True}),
+                                 (.01, {"decay_epochs": 25, "decay_rate": .1, "staircase": False}),
+                                 (.01, {"decay_epochs": 50, "decay_rate": .1, "staircase": True}),
+                                 (.01, {"decay_epochs": 50, "decay_rate": .1, "staircase": False}),
+                                 (.001, {"decay_epochs": 25, "decay_rate": .1, "staircase": True}),
+                                 (.001, {"decay_epochs": 50, "decay_rate": .1, "staircase": True}),
+                                 (.001, {"decay_epochs": 50, "decay_rate": .1, "staircase": False}),
+                                 (.001, {}), (.00001, {})]
 
-    op_fun_and_kwargss = [("adam", {}), ("grad", {}), ("rmsprop", {}), ("rmsprop", {}), ("rmsprop", {})]
+    op_fun_and_kwargss = [("adam", {}), ("grad", {}), ("rmsprop", {})]
     weight_inits = ["default","He","Xnormal"]
     act_fns = ["lrelu"]
-    act_leak_probs = [0.0,0.2,0.2,0.4,0.6]
+    act_leak_probs = [0.0,0.2,0.4,0.6]
 
     hist_eqs = [False]
 
@@ -38,7 +56,7 @@ def get_hyp_opts(all_hyps = ("objective_fns", "tuning_constants", "ss_rs", "regu
                      {"clipLimit": 60.0, "tileGridSize": (8, 8)}]
 
     per_image_normalizations = [False, True]
-    gammas = [1.0,2.0,6.0]
+    gammas = [1.0,2.0,3.0,6.0]
 
     seqs = [None]
 
@@ -84,15 +102,23 @@ def get_hyp_opts(all_hyps = ("objective_fns", "tuning_constants", "ss_rs", "regu
     return all_hyps_opts, all_hyps
 
 if __name__ == '__main__':
-    EXPERIMENTS_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/second round hyp results/overall results"
-    OUTPUT_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/second round hyp results/overall_output"
-    TOP_EXPERIMENTS_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/second round hyp results/top overall results"
-    TOP_OUTPUT_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/second round hyp results/top overall output"
+    EXPERIMENTS_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/round0_4_and_6_1/overall results"
+    OUTPUT_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/round0_4_and_6_1/overall output"
+    TOP_EXPERIMENTS_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/round0_4_and_6_1/top overall results"
+    TOP_OUTPUT_DIR_PATH = "/Users/arvind.m.vepa/Documents/vessel segmentation/round0_4_and_6_1/top overall output"
 
-    jobs = analyze(EXPERIMENTS_DIR_PATH=EXPERIMENTS_DIR_PATH, OUTPUT_DIR_PATH=OUTPUT_DIR_PATH, get_hyp_opts=get_hyp_opts)
+    jobs = analyze(EXPERIMENTS_DIR_PATH=EXPERIMENTS_DIR_PATH, OUTPUT_DIR_PATH=OUTPUT_DIR_PATH, get_hyp_opts=get_hyp_opts, rt_jobs_score=.97)
     copy_jobs(jobs, EXPERIMENTS_DIR_PATH, TOP_EXPERIMENTS_DIR_PATH)
-    analyze(EXPERIMENTS_DIR_PATH=TOP_EXPERIMENTS_DIR_PATH, OUTPUT_DIR_PATH=TOP_OUTPUT_DIR_PATH, get_hyp_opts=get_hyp_opts)
 
+    # save jobs that score above .970 AUC
+    with open("top_job_opts.csv", "a") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for job in jobs:
+            job_opts = get_job_opts(job)
+            print(get_job_kwargs_from_job_opts(job_opts))
+            writer.writerow(job_opts)
+
+    analyze(EXPERIMENTS_DIR_PATH=TOP_EXPERIMENTS_DIR_PATH, OUTPUT_DIR_PATH=TOP_OUTPUT_DIR_PATH, get_hyp_opts=get_hyp_opts)
 
 
 
--- utilities/misc.py
+++ utilities/misc.py
@@ -7,6 +7,8 @@ import csv
 from statsmodels import robust
 from collections import defaultdict
 import re
+from copy import deepcopy
+import json
 
 
 def find_closest_pos(positions, start_pos=(0,0)):
@@ -60,10 +62,131 @@ def copy_stuff(src, dst):
             shutil.copy(src, dst)
         else: raise
 
-def copy_jobs(jobs, EXPERIMENTS_DIR_PATH, TOP_EXPERIMENTS_DIR_PATH, top=12):
+def get_experiment_string(objective_fn,tuning_constant,ss_r,regularizer_args,op_fun_and_kwargs,
+                          learning_rate_and_kwargs, weight_init, act_fn, act_leak_prob, seq, hist_eq, clahe_kwargs,
+                          per_image_normalization,gamma, sep="__"):
+    exp_string = ""
+    exp_string += objective_fn + sep
+    exp_string += (str(tuning_constant) if objective_fn !="ss" else str(None)) + sep
+    exp_string += (str(ss_r) if objective_fn=="ss" else str(None)) + sep
+    exp_string += str(regularizer_args) + sep
+    exp_string += str(op_fun_and_kwargs) + sep
+
+    learning_rate_and_kwargs = deepcopy(learning_rate_and_kwargs)
+    learning_rate_kwargs = learning_rate_and_kwargs[1]
+    if "decay_epochs" in learning_rate_kwargs:
+        learning_rate_kwargs["d_e"]=learning_rate_kwargs.pop("decay_epochs")
+    if "decay_rate" in learning_rate_kwargs:
+        learning_rate_kwargs["d_r"] = learning_rate_kwargs.pop("decay_rate")
+    if "staircase" in learning_rate_kwargs:
+        learning_rate_kwargs["s_c"] = learning_rate_kwargs.pop("staircase")
+    exp_string += "("+str(learning_rate_and_kwargs[0]) +","+json.dumps(learning_rate_kwargs, sort_keys=True) + ")"+sep
+
+    exp_string += weight_init + sep
+    exp_string += act_fn + sep
+    exp_string += str(act_leak_prob) + sep
+    exp_string += str(seq) + sep
+    exp_string += str(hist_eq) + sep
+
+    clahe_kwargs = deepcopy(clahe_kwargs)
+    if clahe_kwargs is None:
+        exp_string += str(None) + sep
+    else:
+        clahe_kwargs["cl"] = clahe_kwargs.pop("clipLimit")
+        clahe_kwargs["tgs"] = clahe_kwargs.pop("tileGridSize")
+        exp_string += json.dumps(clahe_kwargs, sort_keys=True) + sep
+
+    exp_string += str(per_image_normalization) + sep
+    exp_string += str(gamma)
+    exp_string = exp_string.replace("\'","").replace("\"","").replace(",","").replace(" ","-").\
+        replace("{","(").replace("}",")").replace("[","(").replace("]",")").replace(":","-")
+
+    #exp_string = exp_string.replace("\\", "").replace('/', '')
+    return exp_string
+
+def get_job_kwargs_from_job_opts(job_opts):
+    job_dict = dict()
+    job_dict["objective_fn"] = job_opts[0]
+    job_dict["tuning_constant"] = None if job_opts[1] == "None" else float(job_opts[1])
+    job_dict["ss_r"] =  None if job_opts[2] == "None" else float(job_opts[2])
+
+    if job_opts[3] == "None":
+        job_dict["regularizer_args"] = None
+    else:
+        reg_str = job_opts[3][1:len(job_opts[3])-1]
+        sep_index = reg_str.find("-")
+        reg_list = [reg_str[:sep_index], reg_str[sep_index+1:]]
+        reg_list[1] = float(reg_list[1])
+        job_dict["regularizer_args"] = tuple(reg_list)
+
+    opt_list = job_opts[4].replace("-", ",").replace(")","").replace("(","").split(",")
+    opt_list[1]={}
+    job_dict["op_fun_and_kwargs"] = tuple(opt_list)
+
+    lr_str = job_opts[5][1:len(job_opts[5])-1]
+    lr_list = lr_str.split("(")
+    lr_list[0] = float(lr_list[0])
+    lr_list[1] = lr_list[1].replace(")","")
+    if lr_list[1] == "":
+        lr_list[1] = {}
+    else:
+        decay_epochs = 'd_e--'
+        decay_rate = '-d_r--'
+        staircase = '-s_c--'
+        de_index = lr_list[1].find(decay_epochs)
+        dr_index = lr_list[1].find(decay_rate)
+        sc_index = lr_list[1].find(staircase)
+        lr_list[1] = {"decay_epochs": int(lr_list[1][de_index+len(decay_epochs):dr_index]),
+                      "decay_rate": float(lr_list[1][dr_index+len(decay_rate):sc_index]),
+                      "staircase": lr_list[1][sc_index+len(staircase):] == "true"}
+    job_dict["learning_rate_and_kwargs"] = tuple(lr_list)
+
+    job_dict["weight_init"] = job_opts[6]
+    job_dict["act_fn"] = job_opts[7]
+    job_dict["act_leak_prob"] = float(job_opts[8])
+    job_dict["seq"] = None
+    job_dict["hist_eq"] = job_opts[10] == 'True'
+
+    if job_opts[11] == "None":
+        job_dict["clahe_kwargs"] = None
+    else:
+        ck_str = job_opts[11][1:len(job_opts[11]) - 1]
+        clipLimit = "cl--"
+        tileGridSize = "-tgs--"
+        cl_index = ck_str.find(clipLimit)
+        tgs_index = ck_str.find(tileGridSize)
+        cl = float(ck_str[cl_index+len(clipLimit):tgs_index])
+        tgs = ck_str[tgs_index+len(tileGridSize):]
+        tgs = tgs[1:len(tgs)-1].split("-")
+        tgs = tuple([int(tgs_) for tgs_ in tgs])
+        job_dict["clahe_kwargs"] = {"clipLimit": cl,
+                                    "tileGridSize": tgs}
+
+    job_dict["per_image_normalization"] = job_opts[12] == 'True'
+    job_dict["gamma"] = float(job_opts[13])
+    return job_dict
+
+def get_job_opts(job_file):
+    job_opts = job_file.split("__")
+    if len(job_opts) == 13:
+        if "(" in job_opts[2]:
+            pos = job_opts[2].find("(")
+        elif "N" in job_opts[2]:
+            pos = job_opts[2].find("N")
+        arg0 = job_opts[2][:pos]
+        arg1 = job_opts[2][pos:]
+        job_opts[2] = arg0
+        job_opts.insert(3, arg1)
+    return job_opts
+
+
+def copy_jobs(jobs, EXPERIMENTS_DIR_PATH, TOP_EXPERIMENTS_DIR_PATH, top=None):
     if not os.path.exists(TOP_EXPERIMENTS_DIR_PATH):
         os.makedirs(TOP_EXPERIMENTS_DIR_PATH)
-    filtered_jobs = jobs[:top]
+    if top is not None:
+        filtered_jobs = jobs[:top]
+    else:
+        filtered_jobs = jobs
     for job in filtered_jobs:
         cur_loc = os.path.join(EXPERIMENTS_DIR_PATH, job)
         new_loc = os.path.join(TOP_EXPERIMENTS_DIR_PATH, job)
@@ -72,8 +195,8 @@ def copy_jobs(jobs, EXPERIMENTS_DIR_PATH, TOP_EXPERIMENTS_DIR_PATH, top=12):
 def analyze(relevant_hyps = ("objective_fns", "tuning_constants", "ss_rs", "regularizer_argss",
                              "op_fun_and_kwargss","learning_rate_and_kwargss","weight_inits","act_fns","act_leak_probs",
                              "seqs","hist_eqs","clahe_kwargss", "per_image_normalizations","gammas"), mof_metric="mad",
-            n_metric_intervals=20, EXPERIMENTS_DIR_PATH=".", OUTPUT_DIR_PATH=".", rt_jobs_metric_interval=10,
-            get_hyp_opts = lambda *args, **kwargs: None):
+            n_metric_intervals=20, EXPERIMENTS_DIR_PATH=".", OUTPUT_DIR_PATH=".", rt_jobs_score = None,
+            rt_jobs_metric_interval=None, get_hyp_opts = lambda *args, **kwargs: None):
 
     if not os.path.exists(OUTPUT_DIR_PATH):
         os.makedirs(OUTPUT_DIR_PATH)
@@ -100,16 +223,7 @@ def analyze(relevant_hyps = ("objective_fns", "tuning_constants", "ss_rs", "regu
         job_metrics_file = [file for file in os.listdir(JOB_PATH) if "mof" in file][0]
         JOB_METRICS_PATH = os.path.join(JOB_PATH, job_metrics_file)
         # parse the hyper-parameter options from the file name
-        job_opts = job_file.split("__")
-        if len(job_opts) == 13:
-            if "(" in job_opts[2]:
-                pos = job_opts[2].find("(")
-            elif "N" in job_opts[2]:
-                pos = job_opts[2].find("N")
-            arg0 = job_opts[2][:pos]
-            arg1 = job_opts[2][pos:]
-            job_opts[2] = arg0
-            job_opts.insert(3,arg1)
+        job_opts = get_job_opts(job_file)
 
         auc_col = 1
         p = re.compile(r'\d+\.\d+')
@@ -178,18 +292,23 @@ def analyze(relevant_hyps = ("objective_fns", "tuning_constants", "ss_rs", "regu
     rank_job_log = "rank_job_log.csv"
     rank_job_log_path = os.path.join(OUTPUT_DIR_PATH, rank_job_log)
 
-    rt_jobs = None
+    rt_jobs = []
     with open(rank_job_log_path, "a") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         for i in range(n_metric_intervals):
             i_job_results = job_results[i]
             i_job_results = i_job_results.items()
             i_job_results = sorted(i_job_results, key = lambda x: x[1][0], reverse=True)
-            i_job, i_results = zip(*i_job_results)
+            i_jobs, i_results = zip(*i_job_results)
             mean_results, _ = zip(*i_results)
-            if i == rt_jobs_metric_interval:
-                rt_jobs = i_job
-            writer.writerow(list(i_job)+["mean", mof_metric])
+            if rt_jobs_score is not None and (rt_jobs_metric_interval is None or i == rt_jobs_metric_interval):
+                for i_job, i_result in zip(i_jobs, i_results):
+                    if i_result[0] > rt_jobs_score:
+                        if i_job not in rt_jobs:
+                            rt_jobs.append(i_job)
+            elif rt_jobs_metric_interval is not None and i == rt_jobs_metric_interval:
+                rt_jobs = i_jobs
+            writer.writerow(list(i_jobs)+["mean", mof_metric])
             writer.writerow(list([" +/- ".join([str(metric) for metric in np.round(i_result,4)])+" % rank {:.1%}".format(float(i)/len(i_results)) for i,i_result in enumerate(i_results)])+[np.round(np.mean(mean_results),4),np.round(mof_func(mean_results),4)])
 
     return rt_jobs
