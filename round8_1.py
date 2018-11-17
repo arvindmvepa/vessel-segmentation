e2ca85fbde190fc42105ace6459af29de6a91281
arvind.m.vepa <arvind.m.vepa@gmail.com>
2018-11-17 00:31:22 -0800

    update

--- round8_1.py
+++ round8_1.py
@@ -17,7 +17,7 @@ def get_experiment_string(seq, n_epochs, sep="__"):
     else:
         exp_string += str(seq) + sep
 
-    exp_string += n_epochs
+    exp_string += str(n_epochs)
 
     exp_string = exp_string.replace("\'","").replace("\"","").replace(",","").replace(" ","-").\
         replace("{","(").replace("}",")").replace("[","(").replace("]",")").replace(":","-")
@@ -37,9 +37,10 @@ if __name__ == '__main__':
     metrics_epoch_freq = 5
     viz_layer_epoch_freq = 10001
 
-    WRK_DIR_PATH = "/root/DRIVE"
+    WRK_DIR_PATH = "/root/vessel-segmentation/DRIVE"
     #WRK_DIR_PATH = "C:\\vessel-segmentation\\drive"
     n_splits = 4
+    n_epochs = 100
 
     ### RANDOM SEARCH
     tuning_constant = 1.0
@@ -47,6 +48,7 @@ if __name__ == '__main__':
     regularizer_args = None
 
     op_fun_and_kwargs = ("adam", {})
+    learning_rate_and_kwargs = (.001, {"decay_epochs": 25, "decay_rate": .1, "staircase": True})
     weight_init = "default"
     act_fn = "lrelu"
     act_leak_prob = 0.2
@@ -66,28 +68,26 @@ if __name__ == '__main__':
                             iaa.Fliplr(0.5, name="Flipper")]),
             iaa.Sequential([iaa.Affine(rotate=(-90, 90), mode='constant', cval=0, name="rotate"),
                             iaa.Fliplr(0.5, name="Flipper")]),
-            iaa.Sequential([iaa.Affine(rotate=(-10, 10), mode='constant', cval=0, name="rotate_small")])
-            ]
-    for learning_rate_and_kwargs, n_epochs in [((.001, {"decay_epochs": 25, "decay_rate": .1, "staircase": True}), 100),
-                                               ((.001, {"decay_epochs": 50, "decay_rate": .1, "staircase": True}), 200)]:
-        for seq in seqs:
+            iaa.Sequential([iaa.Affine(rotate=(-10, 10), mode='constant', cval=0, name="rotate_small")])]
 
-            EXPERIMENT_NAME = get_experiment_string(seq, n_epochs)
+    for seq in seqs:
 
-            OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
+        EXPERIMENT_NAME = get_experiment_string(seq, n_epochs)
 
-            job = DriveJob(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
-            job.run_cv(WRK_DIR_PATH=WRK_DIR_PATH, mc=True, val_prop=.10, early_stopping=False, early_stopping_metric="auc",
-                       save_model=False, save_sample_test_images=False,
-                       metrics_epoch_freq=metrics_epoch_freq, viz_layer_epoch_freq=viz_layer_epoch_freq,
-                       n_epochs=n_epochs, n_splits=n_splits, objective_fn=objective_fn,
-                       tuning_constant=tuning_constant,
-                       regularizer_args=regularizer_args,
-                       op_fun_and_kwargs=op_fun_and_kwargs,
-                       learning_rate_and_kwargs=learning_rate_and_kwargs,
-                       weight_init=weight_init, act_fn=act_fn, act_leak_prob=act_leak_prob,
-                       seq=seq, hist_eq=hist_eq,
-                       clahe_kwargs=clahe_kwargs,
-                       per_image_normalization=per_image_normalization,
-                       gamma=gamma)
+        OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
+
+        job = DriveJob(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
+        job.run_cv(WRK_DIR_PATH=WRK_DIR_PATH, mc=True, val_prop=.10, early_stopping=False, early_stopping_metric="auc",
+                   save_model=False, save_sample_test_images=False,
+                   metrics_epoch_freq=metrics_epoch_freq, viz_layer_epoch_freq=viz_layer_epoch_freq,
+                   n_epochs=n_epochs, n_splits=n_splits, objective_fn=objective_fn,
+                   tuning_constant=tuning_constant,
+                   regularizer_args=regularizer_args,
+                   op_fun_and_kwargs=op_fun_and_kwargs,
+                   learning_rate_and_kwargs=learning_rate_and_kwargs,
+                   weight_init=weight_init, act_fn=act_fn, act_leak_prob=act_leak_prob,
+                   seq=seq, hist_eq=hist_eq,
+                   clahe_kwargs=clahe_kwargs,
+                   per_image_normalization=per_image_normalization,
+                   gamma=gamma)
 
