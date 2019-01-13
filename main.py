618195de5cf3b2336e7ecb034181e01c7599691f
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-01-13 00:50:50 -0800

    updated stuff

--- main.py
+++ main.py
@@ -2,60 +2,8 @@ from job.dsa import DsaJob
 from job.drive import DriveJob, DriveCustomJob
 from job.stare import StareJob
 from job.chase import ChaseJob
-from imgaug import augmenters as iaa
-from itertools import product
-from random import sample
 import os
+from hyper_params.hyper_params import run_experiment
 
 if __name__ == '__main__':
-
-    ### OPTIMIZATION
-    op_fun_and_kwargs = ("adam", {})
-    tuning_constant = 1.0
-    ss_r = None
-    objective_fn = "wce"
-    regularizer_args = None
-    learning_rate_and_kwargs = (.001, {})
-
-    ### LAYER ARGS
-    weight_init = "He"
-    act_fn = "lrelu"
-    act_leak_prob = 0.2
-
-    ### IMAGE PRE-PREPROCESSING
-    hist_eq = False
-    clahe_kwargs = None
-    per_image_normalization = False
-    gamma = 1.0
-
-    ### IMAGE AUGMENTATION
-    seq = None
-
-    ### JOB INFO
-    Job_cls = DriveJob
-    WRK_DIR_PATH = "C:\\Users\\arvin\\dev\\vessel-segmentation\\DRIVE"
-    n_splits = 2
-
-    ### OUTPUT INFO
-    EXPERIMENTS_DIR_PATH = "C:\\Users\\arvin\\dev\\vessel-segmentation\\arch_tests"
-
-    EXPERIMENT_NAME = "test"
-    OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
-    metrics_epoch_freq = 1
-    viz_layer_epoch_freq = None
-    n_epochs = 5
-
-    job = Job_cls(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
-    job.run_cv(WRK_DIR_PATH=WRK_DIR_PATH, mc=True, val_prop=.1, save_model=False, save_sample_test_images=False,
-               metrics_epoch_freq=metrics_epoch_freq, viz_layer_epoch_freq=viz_layer_epoch_freq,
-               n_epochs=n_epochs, n_splits=n_splits, objective_fn=objective_fn,
-               tuning_constant=tuning_constant, ss_r=ss_r,
-               regularizer_args=regularizer_args,
-               op_fun_and_kwargs=op_fun_and_kwargs,
-               learning_rate_and_kwargs=learning_rate_and_kwargs,
-               weight_init=weight_init, act_fn=act_fn, act_leak_prob=act_leak_prob,
-               seq=seq, hist_eq=hist_eq,
-               clahe_kwargs=clahe_kwargs,
-               per_image_normalization=per_image_normalization,
-               gamma=gamma)
-
+    run_experiment(exp_file="sample.yml")
---
+++ params/params.py
@@ -0,0 +1,87 @@
+import yaml
+import io
+from utilities.misc import flatten, product_dict, update
+from shutil import copyfile
+from random import sample
+import os
+from job.drive import DriveJob, DriveCustomJob
+from job.stare import StareJob
+from job.chase import ChaseJob
+from job.dsa import DsaJob
+
+job_cls_map = {"DriveJob": DriveJob,
+               "DriveCustomJob": DriveCustomJob,
+               "ChaseJob": ChaseJob,
+               "StareJob": StareJob,
+               "DsaJob": DsaJob,
+               }
+
+
+def run_experiment(exp_file="exp.yml"):
+    job_cls, job_func, EXPERIMENTS_DIR_PATH, WRK_DIR_PATH, exp_params = generate_params(exp_file=exp_file)
+    job_cls = job_cls_map[job_cls]
+
+    # save a copy of the exp.yml file to the experiment directory
+    copyfile(exp_file, os.path.join(EXPERIMENTS_DIR_PATH, os.path.basename(exp_file)))
+
+    exp_base_name = os.path.splitext(exp_file)[0]
+    for i, params in enumerate(exp_params):
+        EXPERIMENT_NAME = exp_base_name+"_"+str(i)
+        OUTPUTS_DIR_PATH = os.path.join(EXPERIMENTS_DIR_PATH, EXPERIMENT_NAME)
+        params_yml = os.path.join(OUTPUTS_DIR_PATH, "params.yml")
+
+        with io.open(params_yml, 'w', encoding='utf8') as outfile:
+            yaml.dump(params, outfile, default_flow_style=False, allow_unicode=True)
+
+        job = job_cls(OUTPUTS_DIR_PATH=OUTPUTS_DIR_PATH)
+        job_func = getattr(job, job_func)
+        job.job_func(WRK_DIR_PATH=WRK_DIR_PATH, **params)
+
+
+def generate_params(exp_file="exp.yml"):
+    """generate the exp params combinations from the dictionary of mappings, with an optional list of choices for params
+    exp"""
+
+    with open(exp_file, 'r') as stream:
+        exp = yaml.load(stream)
+
+    exp = flatten(exp)
+    job_cls = exp.pop("job_cls")
+    job_func = exp.pop("job_func")
+    EXPERIMENTS_DIR_PATH = exp.pop("EXPERIMENTS_DIR_PATH")
+    WRK_DIR_PATH = exp.pop("WRK_DIR_PATH")
+    num_files = exp.pop("num_files", None)
+    
+    fixed_params = dict()
+    testing_params = dict()
+
+    for k,v in exp.items():
+        # params that are stored as list will be distributed
+        # tuples, etc. will not be treated this way
+        if isinstance(v, list):
+            testing_params[k] = v
+        else:
+            fixed_params[k] = v
+
+    # find all the hyper-parameter combinations
+    testing_params = product_dict(testing_params)
+
+    # sample the number of experiments
+    if num_files:
+        testing_params = sample(testing_params, num_files)
+
+    # update the parameter combinations with the fixed parameters
+    params = [update(testing_params_exp, fixed_params) for testing_params_exp in testing_params]
+
+    return job_cls, job_func, EXPERIMENTS_DIR_PATH, WRK_DIR_PATH, params
+
+
+def load_yaml(yaml_file="params.yml"):
+    # load the hyper-params
+    with open(yaml_file, 'r') as stream:
+        data = yaml.load(stream)
+    return data
+
+#def analyze_exp():
+
+
