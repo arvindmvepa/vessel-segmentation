8049efb6d3e0fa4ec25bb5f8660978407242bebd
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-01-13 01:07:25 -0800

    bug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from job.drive import DriveJob, DriveCustomJob
 from job.stare import StareJob
 from job.chase import ChaseJob
 import os
-from hyper_params.hyper_params import run_experiment
+from params.params import run_experiment
 
 if __name__ == '__main__':
     run_experiment(exp_file="sample.yml")
