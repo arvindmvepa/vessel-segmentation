10d69ba4b5afd72c9b8d0186a49e5e391e656dd2
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-01-13 21:54:39 -0800

    test analysis

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from job.drive import DriveJob, DriveCustomJob
 from job.stare import StareJob
 from job.chase import ChaseJob
 import os
-from params.params import run_experiment
+from params.params import run_experiment, analyze_exp
 
 if __name__ == '__main__':
-    run_experiment(exp_file_path="sample.yml")
+    #run_experiment(exp_file_path="sample.yml")
+    analyze_exp(r"C:\Users\arvin\dev\vessel-segmentation\exp_sample")
