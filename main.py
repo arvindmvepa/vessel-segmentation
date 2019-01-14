f972ee1deb83ed382776967574a8f9f3a0fb389d
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-01-13 21:57:02 -0800

    bug

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from params.params import run_experiment, analyze_exp
 
 if __name__ == '__main__':
     #run_experiment(exp_file_path="sample.yml")
-    analyze_exp(r"C:\Users\arvin\dev\vessel-segmentation\exp_sample")
+    analyze_exp(r"C:\Users\arvin\dev\vessel-segmentation\exp_sample", exp_file_name="sample.yml")
