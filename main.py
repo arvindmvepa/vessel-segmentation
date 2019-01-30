a495ffdbb2a37f3b3311c17c23a1f7812474854e
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-01-29 19:35:00 -0800

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utilities.misc import flatten
 from params.misc import copy_exp
 
 if __name__ == '__main__':
-    exp_file_name = "exp_drive_job.yml"
+    exp_file_name = "exp_drive_job_2.yml"
     run_experiment(exp_file_path=exp_file_name, init_count=0)
     TOP_EXPERIMENTS_DIR_PATH = r"/root/vessel-segmentation1/top_drive_jobs"
     jobs = analyze_exp(exp_file_path=exp_file_name, file_char="csv", rt_jobs_score=.96)
