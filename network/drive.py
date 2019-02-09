b75036f4e2d047cd0c444f4382a7563de9079d43
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-02-09 01:10:16 -0800

    new experiment

--- network/drive.py
+++ network/drive.py
@@ -1,6 +1,8 @@
 """This is the file for the DRIVE network subclass"""
 
 from network.small_network import SmallNetwork, SmallNetworkwKerasDecoder
+from network.large_network import LargeNetwork
+
 
 class DriveNetwork(SmallNetwork):
 
@@ -12,6 +14,17 @@ class DriveNetwork(SmallNetwork):
     
     def __init__(self, mask=True,**kwargs):
         super(DriveNetwork, self).__init__(mask=mask,**kwargs)
+
+
+class DriveLargeNetwork(LargeNetwork):
+    IMAGE_HEIGHT = 584
+    IMAGE_WIDTH = 565
+
+    FIT_IMAGE_HEIGHT = 584
+    FIT_IMAGE_WIDTH = 584
+
+    def __init__(self, mask=True, **kwargs):
+        super(DriveNetwork, self).__init__(mask=mask, **kwargs)
 
 
 class DriveCustomNetwork(SmallNetworkwKerasDecoder):
