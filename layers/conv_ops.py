bc09c28da6d34536442dcd1566c06022279e7332
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-02-28 23:09:06 -0800

    add print statement

--- layers/conv_ops.py
+++ layers/conv_ops.py
@@ -27,8 +27,10 @@ class Conv2d(Layer):
         print("name: {}".format(self.name))
         if self.add_to_input:
             input = tf.add(input, include_w_input)
+            print("add to input: {}".format(self.add_to_input))
         if self.concat_to_input:
             input = tf.concat([input, include_w_input],axis=-1)
+            print("concat to input: {}".format(self.concat_to_input))
         self.input_shape = get_incoming_shape(input)
         print(self.input_shape)
         number_of_input_channels = self.input_shape[3]
